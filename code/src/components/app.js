e599f9201d27ee03e0b5757ca77c13dcadbeff20
Emelie Sidesiö <esidesio@gmail.com>
2017-11-27 12:13:11 +0100

    Navigation added

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,9 +5,35 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Find me in src/app.js!
-        <h1>Hello</h1>
-        <h2>Hej igen</h2>
+
+        <header>
+
+          <nav>
+            <ul>
+              <li><a href="#top" className="active">Home</a></li>
+              <li><a href="#top">Technology</a></li>
+              <li><a href="#top">Creativity</a></li>
+              <li><a href="#top">Entrepreneurship</a></li>
+              <li><a href="#top">Self</a></li>
+              <li><a href="#top">Culture</a></li>
+              <li><a href="#top">Digital Design</a></li>
+              <li><a href="#top">Popular on Medium</a></li>
+              <li><a href="#top">Politics</a></li>
+              <li><a href="#top">More</a></li>
+              <li><div className="button">Get started</div></li>
+            </ul>
+          </nav>
+
+        </header>
+
+        <hr></hr>
+
+        <div className="content-blocks">
+
+          <div className="hero-box"></div>
+
+        </div>
+
       </div>
     )
   }
