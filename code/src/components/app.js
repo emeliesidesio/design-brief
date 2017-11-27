ab7fbe9cd729daf2a4a0039c1385afb981559fab
Emelie Sidesiö <esidesio@gmail.com>
2017-11-27 18:32:42 +0100

    Byline image added

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -29,19 +29,13 @@ class App extends React.Component {
           <div className="hero-box">
 
             <div className="hero-copy">
-
               <div className="inner-margin">
                 <h1>Interesting ideas that set your mind in motion.</h1>
                 <h3>Hear directly from the people who know it best. From tech to politics to creativity and more — whatever your interest, we've got you covered.</h3>
               </div>
 
-              <div className="CTA-button CTA-button-1">
-                Get started
-              </div>
-
-              <div className="CTA-button CTA-button-2">
-                Learn more
-              </div>
+              <div className="CTA-button CTA-button-1">Get started</div>
+              <div className="CTA-button CTA-button-2">Learn more</div>
 
             </div>
 
@@ -54,9 +48,7 @@ class App extends React.Component {
           <div className="boxes">
 
             <div className="puff-box">
-              <div className="image-box">
-                <img src="/images/medium-image-puff-1.jpeg" alt="Boy"></img>
-              </div>
+              <div className="image-box" id="image1"></div>
               <div className="copy-box">
                 <div className="inner">
                   <h2>The Boy Who Tamed the Sea</h2>
@@ -66,24 +58,46 @@ class App extends React.Component {
             </div>
 
             <div className="puff-box">
-              <div className="image-box"></div>
-              <div className="copy-box"></div>
+              <div className="image-box" id="image2"></div>
+              <div className="copy-box">
+                <div className="inner">
+                  <h2>The Role of Human Emotions in the Future of Transport</h2>
+                  <p>Getting from A to B is about to feel very different.</p>
+                  <div className="writer">
+                    <div className="writer-image" id="writer-image-2"></div>
+                    <div className="writer-byline">
+                      <h4>Ben Bland</h4>
+                      <h5>Nov 1, 7 min read</h5>
+                    </div>
+                    <div className="bookmark"><img src="/images/bookmark.png" alt="bookmark"></img></div>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div className="puff-box">
-              <div className="image-box"></div>
-              <div className="copy-box"></div>
+              <div className="image-box" id="image3"></div>
+              <div className="copy-box">
+                <div className="inner">
+                  <h2>The March of the Losers</h2>
+                  <p>Around the world, the victims are becoming the abusers, and Donald Trump is their king.</p>
+                </div>
+              </div>
             </div>
 
             <div className="puff-box">
-              <div className="image-box"></div>
-              <div className="copy-box"></div>
+              <div className="image-box" id="image4"></div>
+              <div className="copy-box">
+                <div className="inner">
+                  <h2>The Burning Season</h2>
+                  <p>You cannot save everything when the fires come to Northern California.</p>
+                </div>
+              </div>
             </div>
 
           </div>
 
         </div>
-
 
     </div>
     )
